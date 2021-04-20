667a1dcb8b24c726f734da06279e379dbe9d627f
klord22 <klord@g.hmc.edu>
2021-04-20 16:43:23 -0400

    updated Real Systems

--- Loop_Mass.jl
+++ Loop_Mass.jl
@@ -103,3 +103,46 @@ function massloop_time(m, a2, theta=0, P=5, t="10000P", hParam=0.01, fileSave="A
     q = stabmasslist[maxindex][1]/stabmasslist[maxindex][2]
     return q, stabmasslist
 end
+
+function massloop_a1(m, stab_a1, a2, theta=0, P=5, t="10000P", hParam=0.01, fileSave="AutoSave")
+    #stab_a1 is array of a1's
+    Minner = m[1]
+    Md = m[2]
+    if m[1] < 1.3
+        error("Total inner mass must be higher than 1.3Msun to be greater than one neutron star")
+    elseif m[2]>m[1]
+        error("The donor star must be less massive than the inner system")
+    end
+    M1 = 1.3
+    M2 = Minner - 1.3
+    mstep = (.5*Minner-1.3)/4 #create a step to change the mass ratio by
+    stability = 0
+    stabmasslist = [] #will contain tuples of masses, a1 and stability
+    timelist = split(t, "P") #note: split removes the P
+    while M1 <= M2 +.1 #fudge factor so that will get to equivalent state
+        M = [M1,M2,Md]
+        a1 = StabilityFinder2(M, a2, theta, P, t, hParam, fileSave)
+        grtime = ((a1/1.135)^4)/(M1*M2*(M1+M2))
+        stabletuple = (M1, M2, a1, grtime)
+        push!(stabmasslist, stabletuple)
+        y = open("TimeLoop_$([m[1],m[2],a2]).txt","a") #creating file to record outputs, "a" means append
+        write(y,"a1 is $a1, spiral-in time is $grtime\n")
+        close(y)
+        M1 += mstep
+        M2 = Minner - M1
+    end
+    #find the maximum spiral-in time
+    maxa1 = 0
+    maxtime = 0
+    maxindex = 0
+    for i = 1:5
+        if stabmasslist[i][3] >= maxa1 #if current a1 is >= to the max so far
+            if stabmasslist[i][4] > maxtime #if surpass max spiral-in time
+                maxtime = stabmasslist[i][4]
+                maxindex = i
+            end
+        end
+    end
+    q = stabmasslist[maxindex][1]/stabmasslist[maxindex][2]
+    return q, stabmasslist
+end
